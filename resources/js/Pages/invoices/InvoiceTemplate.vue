91d69ea2f530d8be748669a92d7819674e598aeb
manish kumar <manish@zolacar.com>
2024-04-16 13:51:10 +0530

    invoice changes

--- resources/js/Pages/invoices/InvoiceTemplate.vue
+++ resources/js/Pages/invoices/InvoiceTemplate.vue
@@ -211,7 +211,7 @@
                 </p>
             </div>
             <div class="font-bold border-b border-l border-r border-black border-solid col-6">
-                <img class="img" :src="props.invoice.company.sign ?? ''" alt="jpeg" style="height:150px;width:300px" />
+                <img class="img" :src="props.invoice.company.sign ?? ''" alt="jpeg" style="height:100px;width:250px" />
             </div>
         </div>
     </div>
