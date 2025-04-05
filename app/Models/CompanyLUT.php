34b85d78cc6c299c0b7771e31cddf0ff52b0b0d0
Manish <softpromani@gmail.com>
2025-04-05 21:25:10 +0530

    lut expiry

--- app/Models/CompanyLUT.php
+++ app/Models/CompanyLUT.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Support\Carbon;
 class CompanyLUT extends Model
 {
     use HasFactory,SoftDeletes;
     protected $guarded = [];
+    protected $appends = ['formatted_expiry_date'];
+    public function getFormattedExpiryDateAttribute()
+{
+    return $this->expiry_date ? Carbon::parse($this->expiry_date)->format('d-M-Y') : null;
+}
 }
