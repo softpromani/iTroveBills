1a26ea78e3379b9db63acd6facac5f7385ee517a
softpromani <softpromani@gmail.com>
2025-09-03 23:41:46 +0530

    changes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -653,7 +653,7 @@ const mobileMenuOpen = ref(false);
                             <svg class="w-5 h-5 mr-3 text-purple-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                             </svg>
-                            <span class="text-gray-300">+91 9646061515 (India), +975 17126364 (Bhutan)</span>
+                            <span class="text-gray-300">+91 9648061515 (India), +975 17126364 (Bhutan)</span>
                         </li>
                         <li class="flex items-start">
                             <svg class="w-5 h-5 mr-3 mt-1 text-purple-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
